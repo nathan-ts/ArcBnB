abc1b3240547994af03cf2109bb71d9dcb8a52bd
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-04-07 03:30:17 +0000

    add webapp

--- 1_queries/5_my_reservations.sql
+++ 1_queries/5_my_reservations.sql
@@ -1,11 +1,3 @@
--- SELECT reservations.id, properties.title, start_date, cost_per_night, AVG(rating)
--- FROM properties JOIN reservations ON property_id = properties.id
--- JOIN property_reviews ON reservation_id = reservations.id
--- WHERE reservations.guest_id = 1
--- GROUP BY reservations.id, properties.title, start_date, cost_per_night
--- ORDER BY start_date
--- LIMIT 10;
-
 SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
